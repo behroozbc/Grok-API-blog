44576ca4ca94f478f91cd589684b0b99cd1ca67a
behrooz bozorg chami <behroozbc@outlook.com>
2024-12-12 18:56:10 +0100

    Add project files.

---
+++ Grok-API-blog/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+using OpenAI;
+
+
+var xAI_key = new ConfigurationBuilder().AddUserSecrets<Program>().Build().GetValue<string>("xAI-key") ?? "";
+
+var client = (new OpenAIClient(new(xAI_key), new() { Endpoint = new("https://api.x.ai/v1") })).GetChatClient("grok-beta");
+List<ChatMessage> messages =
+[
+    new SystemChatMessage(File.ReadAllText("systemIntialMessage.txt")),
+    new UserChatMessage(File.ReadAllText("userMessage.txt")),
+];
+
+ChatCompletion completion = client.CompleteChat(messages);
+File.WriteAllText("grok-response.txt",completion.Content[0].Text);
